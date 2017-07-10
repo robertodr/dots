482e65db437ae88bf6792f4cb97ee7352e8f798e
Roberto Di Remigio <roberto.diremigio@gmail.com>
2017-07-10 08:51:59 +0200

    Make files hidden when linking

--- default.nix
+++ default.nix
@@ -9,8 +9,8 @@ in
   dotfiles = mkHome {
     user = builtins.getEnv "USER";
     files = {
-      ".bcrc" = inHome ".dots/.bcrc";
-      ".gitconfig" = inHome ".dots/.gitconfig";
+      ".bcrc" = inHome ".dots/bcrc";
+      ".gitconfig" = inHome ".dots/gitconfig";
       ".config/fish/config.fish" = inHome ".dots/config.fish";
       ".config/fish/aliases.fish" = inHome ".dots/aliases.fish";
       ".config/fish/functions" = inHome ".dots/functions";
