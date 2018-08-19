731e00a1c4d0518b890a6afd17ab94979debcf58
Roberto Di Remigio <roberto.diremigio@gmail.com>
2018-08-19 11:57:42 -0400

    Simplify default.nix

--- default.nix
+++ default.nix
@@ -19,36 +19,21 @@ in
       # Desktop background
       ".background-image" = inHome ".dots/pulse-demon.png";
       # Awesome
-      ".config/awesome/rc.lua" = inHome ".dots/config/awesome/rc.lua";
+      ".config/awesome" = inHome ".dots/config/awesome";
       # direnv
-      ".config/direnv/direnvrc" = inHome ".dots/config/direnv/direnvrc";
+      ".config/direnv" = inHome ".dots/config/direnv";
       # fish
-      ".config/fish/config.fish" = inHome ".dots/config/fish/config.fish";
-      ".config/fish/aliases.fish" = inHome ".dots/config/fish/aliases.fish";
-      ".config/fish/functions/fish_prompt.fish" = inHome ".dots/config/fish/functions/fish_prompt.fish";
-      ".config/fish/functions/fish_right_prompt.fish" = inHome ".dots/config/fish/functions/fish_right_prompt.fish";
-      ".config/fish/functions/abspath.fish" = inHome ".dots/config/fish/functions/abspath.fish";
-      ".config/fish/functions/ls.fish" = inHome ".dots/config/fish/functions/ls.fish";
-      ".config/fish/functions/wttr.in.fish" = inHome ".dots/config/fish/functions/wttr.in.fish";
-      ".config/fish/misc/nix_fish_env.sh" = inHome ".dots/config/fish/misc/nix_fish_env.sh";
-      # Kitty
-      ".config/kitty/kitty.conf" = inHome ".dots/config/kitty/kitty.conf";
-      ".config/kitty/kitty.session" = inHome ".dots/config/kitty/kitty.session";
+      ".config/fish" = inHome ".dots/config/fish";
+      # kitty
+      ".config/kitty" = inHome ".dots/config/kitty";
       # Neovim
-      ".config/nvim/init.vim" = inHome ".dots/config/nvim/init.vim";
-      ".config/nvim/nvimrc.before" = inHome ".dots/config/nvim/nvimrc.before";
-      ".config/nvim/nvimrc.plugins" = inHome ".dots/config/nvim/nvimrc.plugins";
+      ".config/nvim" = inHome ".dots/config/nvim";
       # Spacemacs
       ".spacemacs" = inHome ".dots/spacemacs";
       # Stack
-      ".stack/config.yaml" = inHome ".dots/stack/config.yaml";
-      ".stack/shell.nix" = inHome ".dots/stack/shell.nix";
+      ".stack" = inHome ".dots/stack";
       # Liferea
-      ".config/liferea/accels" = inHome ".dots/config/liferea/accels";
-      ".config/liferea/cookies.txt" = inHome ".dots/config/liferea/cookies.txt";
-      ".config/liferea/feedlist.opml" = inHome ".dots/config/liferea/feedlist.opml";
-      ".config/liferea/feedlist.opml.backup" = inHome ".dots/config/liferea/feedlist.opml.backup";
-      ".config/liferea/liferea.css" = inHome ".dots/config/liferea/liferea.css";
+      ".config/liferea" = inHome ".dots/config/liferea";
       ".local/share/liferea/liferea.db" = inHome ".dots/local/share/liferea/liferea.db";
     };
   };
